759439cd470e4f0a855144c53c22ab4c68748c91
Sinisa <sinisa@hackandcraft.com>
2014-11-16 13:50:35 +0100

    stripDiacritics filter refactored

--- src/filters/diacriticFilter.js
+++ src/filters/diacriticFilter.js
@@ -3,10 +3,6 @@ angular
 	.filter('diacriticStrip', ['DiacriticsRemoval',
 		function(DiacriticsRemoval){
 			return function(input){
-				var map = DiacriticsRemoval.getList();
-				for(var i=0; i<map.length; i++) {
-					input = input.replace(map[i].letters, map[i].base);
-				}
-				return input;
+				return DiacriticsRemoval.stripDiacritics(input);
 			} 
 		}]);
--- src/services/diacriticsRemoval.js
+++ src/services/diacriticsRemoval.js
@@ -1,7 +1,7 @@
 angular
 	.module('sinisag.service.DiacriticsRemoval', [])
 	.service('DiacriticsRemoval', function() {
-		function Map() {
+		function Removal() {
 			this.DiacriticsRemovalMap = [
 				{'base':'A', 'letters':/[\u0041\u24B6\uFF21\u00C0\u00C1\u00C2\u1EA6\u1EA4\u1EAA\u1EA8\u00C3\u0100\u0102\u1EB0\u1EAE\u1EB4\u1EB2\u0226\u01E0\u00C4\u01DE\u1EA2\u00C5\u01FA\u01CD\u0200\u0202\u1EA0\u1EAC\u1EB6\u1E00\u0104\u023A\u2C6F]/g},
 				{'base':'AA','letters':/[\uA732]/g},
@@ -89,8 +89,14 @@ angular
 				{'base':'z','letters':/[\u007A\u24E9\uFF5A\u017A\u1E91\u017C\u017E\u1E93\u1E95\u01B6\u0225\u0240\u2C6C\uA763]/g}
 			];
 		}
-		Map.prototype.getList = function (){
-			return this.DiacriticsRemovalMap;
+
+		Removal.prototype.stripDiacritics = function (input){
+			var map = this.DiacriticsRemovalMap;
+			for(var i=0; i<map.length; i++) {
+				input = input.replace(map[i].letters, map[i].base);
+			}
+			return input;
 		}
-		return new Map();
+		
+		return new Removal();
 	});
