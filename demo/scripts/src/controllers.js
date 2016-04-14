7832b956f1da005239d525d42cd939b6c90e8b72
Maxime VALY <maxime.valy@gmail.com>
2016-04-14 09:48:23 +0200

    First version of `responsiveImage`

--- demo/scripts/src/controllers.js
+++ demo/scripts/src/controllers.js
@@ -4,9 +4,37 @@ demoControllers.controller('baseCtrl', function () {});
 
 demoControllers.controller('directiveCtrl', ['$scope', '$window', 'EventEnumerator', function ($scope, $window, EventEnumerator) {
     $scope.imageUrl = 'http://placehold.it/700x300';
-    $scope.$on(EventEnumerator.imgChangeSuccess, function () {
+    $scope.$on(EventEnumerator.imgChangeSuccess, function (e) {
         $window.alert('Img successfully changed');
     });
+
+    var imagesAsArray = [{
+        url: '/images/bikes-400.jpg',
+        width: '400'
+    }, {
+        url: '/images/bikes-800.jpg',
+        width: '800'
+    }, {
+        url: '/images/bikes-1280.jpg',
+        width: '1280'
+    }, {
+        url: '/images/bikes-2560.jpg',
+        width: '2560'
+    }];
+
+    var sizes = [
+        "(max-width: 320px) 400px", // media + size (px or vw)
+        "(max-width: 800px) 800px",
+        "(max-width: 1440px) 1280px",
+        "2560px"
+    ];
+
+    $scope.image = {
+        placeholder: imagesAsArray[0].url,
+        fallback: 'http://placehold.it/700x300',
+        images: imagesAsArray,
+        sizes: sizes
+    };
 }]);
 
 demoControllers.controller('filterCtrl', ['$scope', function ($scope) {
--- src/directives/directives.js
+++ src/directives/directives.js
@@ -1,3 +1,4 @@
 angular
     .module('onefootball.components.directives',
-        ['onefootball.components.directives.imgChange']);
+        ['onefootball.components.directives.imgChange',
+            'onefootball.components.directives.responsiveImage']);
